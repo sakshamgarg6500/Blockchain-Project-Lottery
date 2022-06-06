02b60a3e212c647ffbc5831b2cc6184ac3ead2d8
Saksham Garg <sakshamgarg4957@gmail.com>
2022-06-06 13:15:55 +0530

    First Commit

---
+++ src/lottery.js
@@ -0,0 +1,73 @@
+import Web3 from "web3";
+const web3 = new Web3(window.ethereum);
+
+//deployed contract address
+const address = "0x0Fe2ea2e54A8B3D41fF8e5Bf538d55828Ea1ACA8";
+
+//deployed contract ABI
+const abi = [
+	{
+		constant: true,
+		inputs: [],
+		name: "manager",
+		outputs: [{ name: "", type: "address" }],
+		payable: false,
+		stateMutability: "view",
+		type: "function",
+	},
+	{
+		constant: false,
+		inputs: [],
+		name: "pickWinner",
+		outputs: [],
+		payable: false,
+		stateMutability: "nonpayable",
+		type: "function",
+	},
+	{
+		constant: true,
+		inputs: [],
+		name: "spitBalance",
+		outputs: [{ name: "", type: "uint256" }],
+		payable: false,
+		stateMutability: "view",
+		type: "function",
+	},
+	{
+		constant: true,
+		inputs: [],
+		name: "returnPlayers",
+		outputs: [{ name: "", type: "address[]" }],
+		payable: false,
+		stateMutability: "view",
+		type: "function",
+	},
+	{
+		constant: false,
+		inputs: [],
+		name: "enter",
+		outputs: [],
+		payable: true,
+		stateMutability: "payable",
+		type: "function",
+	},
+	{
+		constant: true,
+		inputs: [{ name: "", type: "uint256" }],
+		name: "players",
+		outputs: [{ name: "", type: "address" }],
+		payable: false,
+		stateMutability: "view",
+		type: "function",
+	},
+	{
+		inputs: [],
+		payable: false,
+		stateMutability: "nonpayable",
+		type: "constructor",
+	},
+];
+
+//this creates a copy of our contract that exists on the blockchain
+var contract = new web3.eth.Contract(abi, address);
+export default contract;
